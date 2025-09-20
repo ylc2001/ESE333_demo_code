7a1db173ef1d00e8c3f309f9c0628598fe0067d5
ylc2001 <lichen.yang@stonybrook.edu>
2025-09-19 23:04:39 -0400

    fix shell template

--- basic_demo/shell_template.c
+++ basic_demo/shell_template.c
@@ -28,18 +28,19 @@ void  execute(char **argv) //write your code here
      int    status;
 }
 
-void  main(void)
+int main(void)
 {
-     char  line[1024];             /* the input line                 */
-     char  *argv[64];              /* the command line argument      */
+    char line[1024];             /* the input line                 */
+    char *argv[64];              /* the command line argument      */
 
-     while (1) {                   /* repeat until done ....         */
-          fputs("Shell -> ",stdout);     /*   display a prompt             */
-          fgets(line, 1024, stdin);              /*   read in the command line     */
-          fputs("\n", stdout);
-          parse(line, argv);       /*   parse the line               */
-          if (strcmp(argv[0], "exit") == 0)  /* is it an "exit"?     */
-               exit(0);            /*   exit if it is                */
-          execute(argv);           /* otherwise, execute the command */
-     }
+    while (1) {                   /* repeat until done ....         */
+        fputs("Shell -> ",stdout);     /*   display a prompt             */
+        fgets(line, 1024, stdin);              /*   read in the command line     */
+        fputs("\n", stdout);
+        parse(line, argv);       /*   parse the line               */
+        if (strcmp(argv[0], "exit") == 0)  /* is it an "exit"?     */
+            exit(0);            /*   exit if it is                */
+        execute(argv);           /* otherwise, execute the command */
+    }
+    return 0;
 }
